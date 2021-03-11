5b1dfacc8082eb84ee026d677bdcf868cb65e3e0
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-03-11 20:59:47 +0300

    adding-requrements

--- ecommerceapi/users/serializers.py
+++ ecommerceapi/users/serializers.py
@@ -12,7 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id',  'username', 'email', 'name', 'isAdmin']
 
-    # i do not have a filed isAdmin but i want it included in my response
+    # i do not have a field isAdmin but i want it included in my response
     def get_isAdmin(self, obj):
         return obj.is_staff
 
@@ -30,24 +30,6 @@ class UserSerializerWithToken(UserSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'name', 'isAdmin', 'token']
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return User.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
 
     def get_token(self, obj):
         token = RefreshToken.for_user(obj)
