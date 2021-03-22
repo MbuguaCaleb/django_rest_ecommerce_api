554f25c8b5b204927420d326ec2d38815d10b60d
MbuguaCaleb <mbuguacaleb30@gmail.com>
2021-03-22 08:39:41 +0300

    deploy

--- ecommerceapi/ecommerceapi/settings.py
+++ ecommerceapi/ecommerceapi/settings.py
@@ -88,6 +88,7 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -181,23 +182,12 @@ AFRICAS_TALKING_API_KEY = '59a9a1674cb64f9d65c380bb3ae6ccd0326eb833bcca0e43cdbef
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 # Acessing my Whole Project Path
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
-
-# # enabling whitenoise settings
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
+
+# enabling whitenoise settings
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
